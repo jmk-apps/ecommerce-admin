8beb4394ebf663f220333097241734790b54ac96
John Kaganda <jkaganda@gmail.com>
2025-04-06 18:10:02 +0200

    Add dashboard and setup layouts, implement store retrieval, and enhance store modal functionality

---
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -0,0 +1,28 @@
+import prismadb from '@/lib/prismadb'
+import React from 'react'
+
+interface DashboardPageProps {
+  params: { storeId: string }
+}
+
+
+const DashboardPage: React.FC<DashboardPageProps> = async ({
+  params
+}) => {
+
+  const { storeId } = await params
+
+  const store = await prismadb.store.findFirst({
+    where: {
+      id: storeId
+    }
+  })
+
+  return (
+    <div>
+        Active Store: {store?.name}
+    </div>
+  )
+}
+
+export default DashboardPage
