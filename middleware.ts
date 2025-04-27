13bf7192409b7cfefc46ff56c614f36d4cb77863
John Kaganda <jkaganda@gmail.com>
2025-04-27 19:17:06 +0200

    Add ApiList component and integrate API calls for billboards; update middleware to include API routes as public

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,9 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/api/:path*'])
 
 export default clerkMiddleware(async (auth, req) => {
+  
   if (!isPublicRoute(req)) {
     await auth.protect()
   }
